74a4ff9265242e6ec4fde3a5ce214ef9dc3fff31
efraxpc <efraxpc@gmail.com>
2015-03-11 07:06:42 -0430

    404 error page extra image setted2

--- app/views/backend/errors/home_404.blade.php
+++ app/views/backend/errors/home_404.blade.php
@@ -6,22 +6,22 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 -->
 <!DOCTYPE HTML>
 <html>
-<head>
-<title>Error página no encontrada</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<link rel="stylesheet" href="assets/css/style_404.css">
-</head>
-<body>
-<div class="wrap">
-	<h1>Bikestation.co</h1>
-	<div class="banner">
-		<img src="assets/img/404/banner.png" alt="" />
-	</div>
-	<div class="page">
-		<h2>¡Error página no encontrada!</h2><small>¡Por favor comuníquese con el administrador!</small>
-	</div>
-</div>
-</body>
+	<head>
+		<title>Error página no encontrada</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		{{ HTML::style('assets/css/style_404.css') }}
+	</head>
+	<body>
+		<div class="wrap">
+			<h1>Bikestation.co</h1>
+			<div class="banner">
+				{{ HTML::image('assets/img/404/banner.png') }}
+			</div>
+			<div class="page">
+				<h2>¡Error página no encontrada!</h2><small>¡Por favor comuníquese con el administrador!</small>
+			</div>
+		</div>
+	</body>
 </html>
 
